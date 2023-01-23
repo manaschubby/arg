ae6e8164a54582f51b88c80ebb9ce4cc230de5eb
manaschubby <manaschubby@gmail.com>
2023-01-23 18:33:24 +0530

    Readability reduced

--- pages/login/index.js
+++ pages/login/index.js
@@ -8,51 +8,8 @@ const Index = () => {
     const [level, setLevel] = useState(1);
     const [isLoading, setIsLoading] = useState(true);
     let timeout;
-    const fetch =()=>{
-        Papa.parse("https://docs.google.com/spreadsheets/d/e/2PACX-1vQk1zlmIKcmbHEUfScquMDoTqpzYcan__KpkXx64cJuCUovsbp4Cftl2qAn79GybuaBHrrXLkERoDDU/pub?gid=0&single=true&output=csv",{
-        download: true,
-        hearder:true,
-        complete:(results)=>{
-            setData(results.data);
-            setIsLoading(false);
-        }
-    })
-        setData(Array.from(data))
-    }
-    useEffect(() => {
-        fetch();
-    },[]);
-    const submit = (e)=>{
-        if(passwordRef.current.value==data[level][1]){
-            alert(data[level][2]);
-            
-            if (data[level+1]){
-                setLevel(level+1)
-            }else{
-                    alert("Thank u for playing");
-                    setLevel(1);
-                    return;
-            }
-            setIsLoading(true);
-            timeout=setTimeout(()=>{
-                setIsLoading(false);
-            },2000)
-            
-            passwordRef.current.value="";
-        }
-        else{
-            alert("Incorrect Answer try Again");
-        }
-    }
-    return (
-        <div className='background'>
-            <SVG />
-            {!isLoading ? <div className='center-main'>
-                <h1 className='level'>Level: {level}</h1>
-                <div className='label'>Enter Password</div>
-                <input className='password' type="password" placeholder='Password' ref={passwordRef}></input>
-                <button className='submit' onClick={submit}>Advance</button>
-            </div>
+    const fetch =()=>{Papa.parse("https://docs.google.com/spreadsheets/d/e/2PACX-1vQk1zlmIKcmbHEUfScquMDoTqpzYcan__KpkXx64cJuCUovsbp4Cftl2qAn79GybuaBHrrXLkERoDDU/pub?gid=0&single=true&output=csv",{download: true,hearder:true,complete:(results)=>{setData(results.data);setIsLoading(false);}});setData(Array.from(data))};useEffect(() => {fetch();},[]);const submit = (e)=>{if(passwordRef.current.value==data[level][1]){alert(data[level][2]);if (data[level+1]){setLevel(level+1)}else{alert("Thank u for playing");setLevel(1);return;}setIsLoading(true);timeout=setTimeout(()=>{setIsLoading(false);},2000);passwordRef.current.value="";}
+        else{alert("Incorrect Answer try Again");}};return (<div className='background'><SVG />{!isLoading ? <div className='center-main'><h1 className='level'>Level: {level}</h1><div className='label'>Enter Password</div><input className='password' type="password" placeholder='Password' ref={passwordRef}></input><button className='submit' onClick={submit}>Advance</button> </div>
             : <div className='center-main'><h1 style={{color:"white"}}>Checking Database for answers</h1></div>}
         </div>
     );

7094b619a6bf974d5d8eed0cfc9543f927012079
manaschubby <manaschubby@gmail.com>
2023-01-28 13:44:06 +0530

    ouros bug fix

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import SVG from './ouros/svg'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>ARG</h1>
+    <div className='background'>
+      <SVG />
+      <div className='center-main'>
+        <h1 className='level'>LEVIATHAN</h1>
+        <div className='label'>ALTERNATE REALITY GAME</div>
+      </div>
     </div>
   )
 }
